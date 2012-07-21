d06ca8045323ff9357587aeb2cb9131bbb0dd434
Bob Cousins <bobcousins42@googlemail.com>
2012-07-22 00:37:58 +0100

    Reduce heap size

--- R2C2-RepRap_firmware/FreeRTOSConfig.h
+++ R2C2-RepRap_firmware/FreeRTOSConfig.h
@@ -74,7 +74,7 @@
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 100000000 )
 #define configTICK_RATE_HZ			( ( portTickType ) 1000 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 80 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 19 * 1024 ) )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 12 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 12 )
 #define configUSE_TRACE_FACILITY	1
 #define configUSE_16_BIT_TICKS		0

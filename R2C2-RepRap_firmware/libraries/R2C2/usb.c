ec93eba55c0ee12b5bae3a7fb4ddac901a9b90d2
Casainho <casainho@gmail.com>
2012-09-14 15:24:10 +0100

    Firmware 1.0.2 is now released. Changed USB descriptor string to relect firmware version 1.0.2.

--- R2C2-RepRap_firmware/libraries/R2C2/usb.c
+++ R2C2-RepRap_firmware/libraries/R2C2/usb.c
@@ -96,7 +96,7 @@ static const U8 abDescriptors[] = {
 // configuration descriptor
   0x09,
   DESC_CONFIGURATION,
-  LE_WORD(147),               // wTotalLength
+  LE_WORD(155),               // wTotalLength
   0x02,                       // bNumInterfaces
   0x01,                       // bConfigurationValue
   0x00,                       // iConfiguration
@@ -177,12 +177,13 @@ static const U8 abDescriptors[] = {
 
   58,
   DESC_STRING,
-  'R', 0, '2', 0, 'C', 0, '2', 0, ' ', 0, '-', 0, ' ', 0, //14
+  'R', 0, '2', 0, 'C', 0, '2', 0, ' ', 0, '-', 0, ' ', 0,
   '3', 0, 'D', 0, ' ', 0, 'P', 0, 'r', 0, 'i', 0, 'n', 0, 't', 0, 'e', 0, 'r', 0, ' ', 0, 'C', 0, 'o', 0, 'n', 0,'t', 0,'r', 0,'o', 0,'l', 0,'l', 0,'e', 0,'r', 0, //42
 
-  4,
+  12,
   DESC_STRING,
-  '1', 0,
+  // Version 1.0.2
+  '1', 0, '.', 0, '0', 0, '.', 0, '2', 0,
 
 // terminating zero
   0
